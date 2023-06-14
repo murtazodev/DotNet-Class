376468f71a19abd0675604bfd01a71511ea575fa
murtazodev <toshniyozovmurtazo.r538@gmail.com>
2023-06-14 13:12:40 +0900

    added another class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace task
+{
+    class GenericsExercise
+    {
+        
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world");
+        }
+    }
+}
