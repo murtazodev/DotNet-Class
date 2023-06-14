b9f4448c9f019c34e1d868ddfca4e2bef55073ab
murtazodev <toshniyozovmurtazo.r538@gmail.com>
2023-06-14 14:14:39 +0900

    another line

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 {
     class GenericsExercise
     {
-        
+
     }
     class Program
     {
         public static void Main(string[] args)
         {
             System.Console.WriteLine("Hello world");
+            System.Console.WriteLine("file");
         }
     }
 }
